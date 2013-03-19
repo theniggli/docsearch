110a4b78ca5214b0db2fb73a9497b1a39e0e0771
Fauli <franz.faul@hispeed.ch>
2013-03-19 17:02:10 +0100

    no more Perl ERRORS, but the script is not totally fine yet

--- backend/scripts/indexer.pl
+++ backend/scripts/indexer.pl
@@ -3,69 +3,112 @@
 # initialize all variables
 @documents = ();
 @terms = ();
+%accu = ();
+%queries= ();
+%dNorm = ();
+%idf = ();
+%invIndex = ();
+%nonInvIndex = ();
+@results = ();
+$a = ();
+$qNorm = 0;
 
 #### Main ####
 # all the magic happens here
+# 1. index all files
+
+# 2. calculate all the idfs and so on
+
+# 3. run the queries against the documents
+
+# 4. get the results displayed
+
+# 5. End
 
 
 #### SUBS ####
 #$->Variable , @->Array , %->Hash
+
+sub get_Document_count {
+	$docCount = @documents;
+}
+
 sub indexing_of_documents_and_queries{
-	foreach $doc in @documents { #/create inverted and non-inverted index 
-		get @terms by tokenizing $doc;
-		foreach $term in @terms {
-			%invIndex{$term}{$doc} += 1 # increment frequency in inverted index
-			%nonInvIndex{$doc}{$term} += 1 # increment frequency in non-inv index 
+	foreach $doc (@documents) { #/create inverted and non-inverted index 
+		#get @terms by tokenizing $doc;
+		@terms = split("\s",$doc);
+
+		foreach $term (@terms) {
+			# increment frequency in inverted index
+			$invIndex{$term}{$doc} += 1; 
+			# increment frequency in non inverted index
+			$nonInvIndex{$doc}{$term} += 1; 
 		}
 	}
-	foreach $query in @queries{ #create index of queries
-		get @terms by tokenizing $query; 
-		foreach $term in @terms {
-			%queries{$query}{$term} += 1 # increment frequency 
+
+	#create index of queries
+	foreach $query (@queries){ 
+		#get @terms by tokenizing $query; 
+		@terms = split("\s",$query);
+
+		foreach $term (@terms){
+			# increment frequency
+			$queries{$query}{$term} += 1;
 		}
 	}
 }
 
 sub calculate_all_idfs_and_document_normalizers{
-	foreach $doc in %nonInvIndex{ 
-		%dNorm{$doc} = 0;
-		foreach $word in %nonInvIndex{$doc} {
-			%idf{$word} = log ( (1 + totalNummerOfDocuments) / ( 1 + documentFrequency) ) 
-			$a = %nonInvIndex{$doc}{$word} * %idf{$word}
-			%dNorm{$doc} += $a * $a;
+	foreach $doc (keys  %nonInvIndex) { 
+		$dNorm{$doc} = 0;
+		foreach $word (keys %{$nonInvIndex{$doc}}) { # keys %{$array{Bob}}
+			$idf{$word} = log ( (1 + $docCount) / ( 1 + documentFrequency) );
+			$a = (($nonInvIndex{$doc}{$word}) * ($idf{$word}));
+			$dNorm{$doc} += $a * $a;
 		}
-	%dNorm {$doc} = Math.sqrt(%dNorm{$doc} 
+	$dNorm{$doc} = Math.sqrt($dNorm{$doc});
 	}
 }
 
 sub process_queries{
-	foreach $query in %queryIndex { 
-		$qNorm = 0
-		create new %accu
-		foreach $queryWord in %queryIndex{$query} { # process all query terms
-			if(! %idf{$queryWord}){
-				%idf{$queryWord} = log( 1 + totalNummerOfDocuments);
+	foreach $query (keys %queryIndex) { 
+		$qNorm = 0;
+		#create new %accu;
+		%accu = ();
+		foreach $queryWord (keys %{$queryIndex{$query}}) { # process all query terms
+			if(! $idf{$queryWord}){
+				$idf{$queryWord} = log( 1 + $docCount);
 			}
-			$b = %queries{$query}{$queryWord} * %idf{$queryWord}
-			$qNorm += ($b * $b)
-			if( %invIndex{$queryWord} is definded ) { # if query term occurs in collection
-				foreach $document in @invindex{$queryWord} {
+
+			$b = $queries{$query}{$queryWord} * $idf{$queryWord};
+			$qNorm += ($b * $b);
+			if( defined $invIndex{$queryWord} ) { # if query term occurs in collection
+				foreach $document (keys %{$invIndex{$queryWord}}) {
 					# document scores are added up in accumulators. filename serves as document identifier
-					$a = %invIndex{$queryWord}{$document} * $idf
-					%accu{$document} += ( $a * $b ); 
+					$a = $invIndex{$queryWord}{$document} * $idf;
+					$accu{$document} += ( $a * $b ); 
 				}
 			}
 		} 
+
 		# zweiter Teil 2.2
-		$qNorm = Math.sqrt( $qNorm )
-		foreach $document in %accu { # normalize length of vectors
-			%accu{$document} /= (%dNorm{$document}) * $qNorm )
+		$qNorm = Math.sqrt( $qNorm );
+		foreach $document (keys %accu) { # normalize length of vectors
+			# this used to be weird (%accu{$document} /= (%dNorm{$document}) * $qNorm )), so I removed the "/"
+			$accu{$document} = (($dNorm{$document}) * $qNorm );
 		}
 
-		set @results = sort %accu by values # sort and return 1000 best results
+		#set @results = sort %accu by values 
+		##foreach (sort {$hash{$a} cmp $hash{$b}} (keys %hash);
+		#foreach $key (sort { $ages {$a} <=> $ages {$b}} keys %ages )
+		#{
+		#	print STDOUT "$ages{$key}\n";
+		#}
+
+		# sort and return 1000 best results
 	
-		foreach $result in @results{
-			print "$queryid Q0 $document $rank $accuValue"
+		foreach $result (@results){
+			print "$queryid Q0 $document $rank $accuValue";
 		}
 
 	}
